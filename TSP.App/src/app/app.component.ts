663d9fbfd016282c5f2257d554864e031fe80a38
Ahmad Muahsien <ahmuhaisen03@gmail.com>
2025-01-01 21:20:54 +0300

    Frontend | Tailwind and NgZorro added

--- TSP.App/src/app/app.component.ts
+++ TSP.App/src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NzButtonModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'The Societies Portal';
+
+  messageService = inject(NzMessageService);
+
+  createMessage(){
+    this.messageService.info('The Societies Portal!');
+  }
 }
---
+++ TSP.App/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Work Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
