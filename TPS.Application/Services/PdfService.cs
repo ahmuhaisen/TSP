aae98fd1818f5aef37f1cd2810cfb00d8b533e83
Yasmin <yasmeenalmohtaseb@gmail.com>
2025-02-28 02:35:23 +0300

    Add iText 8.0.5 pdf generator

---
+++ TPS.Application/Services/PdfService.cs
@@ -0,0 +1,24 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout.Element;
+using System.Reflection.Metadata;
+using TPS.Application.Abstractions;
+using iText.Layout;
+namespace TPS.Application.Services
+{
+    public class PdfService : IPdfService
+    {
+        public async Task<byte[]> GeneratePdf(string content)
+        {
+            await Task.Yield();
+            using (var memoryStream = new MemoryStream())
+            using (var writer = new PdfWriter(memoryStream))
+            using (var pdf = new PdfDocument(writer))
+            {
+                var document = new iText.Layout.Document(pdf);
+                document.Add(new Paragraph(content));
+                document.Close();
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
