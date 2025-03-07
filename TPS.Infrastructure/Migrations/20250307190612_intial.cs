fbfbf920e403b67d4778ecbeb6dabedea1777760
suhib <souhibsaleh@outlook.com>
2025-03-08 01:48:27 +0300

    Adding git by month events

--- TPS.Infrastructure/Migrations/20250307190612_intial.cs
+++ TPS.Infrastructure/Migrations/20250307190612_intial.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace TPS.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class Initial : Migration
+    public partial class intial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
